baacec921f5a90017b46de1c686e992ccdbb764a
Ayan Khan <ayank540@gmail.com>
2020-08-13 23:19:41 +0530

    Build Ready

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # OverTheWorldBuild
+This is a news web application that fetches data from various sources and display all at one place.
+
+## Requirements
+To run this project, following things are needed:
+
+* Any modern web browser.
+* Latest version of [Node.js](https://nodejs.org)
+
+## Get started
+Run the following commands:
+
+* Install a minimal local server. In this case, **serve** will be used.
+    ```
+    npm install -g serve
+    ```
+* Next, run following command:
+  ```
+  serve -s build
+  ```
+* Finally, Head over to: ```localhost:5000``` to view the app.
---
+++ build/precache-manifest.50952d884f020e3b4f91d868daa46750.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1b28973dabeac114270a6d119519a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "579a9e0ecc938e2ba491",
+    "url": "/static/css/2.b8a1593a.chunk.css"
+  },
+  {
+    "revision": "8f27ae10d55db2c0a72f",
+    "url": "/static/css/main.1ff4832f.chunk.css"
+  },
+  {
+    "revision": "579a9e0ecc938e2ba491",
+    "url": "/static/js/2.09fdd782.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.09fdd782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f27ae10d55db2c0a72f",
+    "url": "/static/js/main.eabbfa6d.chunk.js"
+  },
+  {
+    "revision": "db47878ca6522bd786ac",
+    "url": "/static/js/runtime-main.6546e3f7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
